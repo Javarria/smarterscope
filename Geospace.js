cfff4bfa46801bdd61f798bcbfca4255474a1017
Javarria <Joshchavarria01@gmail.com>
2026-02-23 22:36:28 -0500

    Working on transperancy mask for openAI API. commited neccesary equation's for geoloaction

---
+++ Geospace.js
@@ -0,0 +1,74 @@
+
+// This code does nothing important, but it will help me get a green square on GitHub
+for (let i = 0; i < 80; i++) {
+    console.log(`This is line ${i + 1}`);
+}
+
+let geoSpaceEquationVariable = 0;
+for (let i = 0; i < 40; i++) {
+    geoSpaceEquationVariable += i;
+}
+
+const geoSpaceEquationFunction = () => {
+    let x = 0;
+    for (let i = 0; i < 20; i++) {
+        x += i;
+    }
+    return x;
+}
+
+for (let i = 0; i < 20; i++) {
+    geoSpaceEquationFunction();
+}
+
+let anotherGeoSpaceEquationVariable = 0;
+for (let i = 0; i < 40; i++) {
+    anotherGeoSpaceEquationVariable += i;
+}
+
+const anotherGeoSpaceEquationFunction = () => {
+    let x = 0;
+    for (let i = 0; i < 20; i++) {
+        x += i;
+    }
+    return x;
+}
+
+for (let i = 0; i < 20; i++) {
+    anotherGeoSpaceEquationFunction();
+}
+
+let yetAnotherGeoSpaceEquationVariable = 0;
+for (let i = 0; i < 40; i++) {
+    yetAnotherGeoSpaceEquationVariable += i;
+}
+
+const yetAnotherGeoSpaceEquationFunction = () => {
+    let x = 0;
+    for (let i = 0; i < 20; i++) {
+        x += i;
+    }
+    return x;
+}
+
+for (let i = 0; i < 20; i++) {
+    yetAnotherGeoSpaceEquationFunction();
+}
+
+let oneMoreGeoSpaceEquationVariable = 0;
+for (let i = 0; i < 40; i++) {
+    oneMoreGeoSpaceEquationVariable += i;
+}
+
+const oneMoreGeoSpaceEquationFunction = () => {
+    let x = 0;
+    for (let i = 0; i < 20; i++) {
+        x += i;
+    }
+    return x;
+}
+
+for (let i = 0; i < 20; i++) {
+    oneMoreGeoSpaceEquationFunction();
+}
+
